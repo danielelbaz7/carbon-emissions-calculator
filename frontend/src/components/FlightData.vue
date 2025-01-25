d5088f50b8d23c64c3252eed2cd2ef7f8921751d
mattbeutel <matthewbeutel@ufl.edu>
2025-01-25 17:56:38 -0500

    Added dropdown and stylization

--- frontend/src/components/FlightData.vue
+++ frontend/src/components/FlightData.vue
@@ -4,7 +4,7 @@
 
 <template>
   <form action="/process-flight-data" method="POST" id="flightForm">
-    <h2>Enter Your Flight Data:</h2>
+    <h2>Have you Flown Recently?</h2>
     <label for="curlocFlight">Departing Airport:</label><br>
     <input type="text" id="curlocFlight"><br>
     <label for="deslocFlight">Destination Airport:</label><br>
--- frontend/src/components/VehicleData.vue
+++ frontend/src/components/VehicleData.vue
@@ -4,11 +4,19 @@
 
 <template>
   <form action="/process-car-data" method="POST" id="carForm">
-    <h2>Enter your Car Information:</h2>
-    <label for="curlocCar">Make:</label><br>
-    <input type="text" id="curlocCar"><br>
+    <h2>Have you Driven Recently?</h2>
+    <label for="curlocCar">Make:</label>
+    <div class="select">
+      <select name="curlocCar" id="curlocCar">
+        <option value="volvo">Volvo</option>
+        <option value="bugatti">Andrew Tate Bugatti</option>
+      </select><br><br>
+      <span class="focus"></span>
+    </div>
     <label for="deslocCar">Model:</label><br>
-    <input type="text" id="deslocCar"><br><br>
+    <input type="text" id="deslocCar"><br>
+    <label for="milDriv">Miles Driven:</label><br>
+    <input type="text" id="milDriv"><br><br>
     <button type="submit">Submit</button>
   </form>
 </template>
